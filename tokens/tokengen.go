d4a12eb570c69dec73c0736269af261cccd811a4
Anish <119729668+Anish2545@users.noreply.github.com>
2024-08-05 16:07:29 +0000

    middleware , cart remaining functions

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,13 +1,17 @@
 package tokens
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/Anish2545/go-ecommerce/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -54,10 +58,51 @@ func TokenGenerator(email string, firstname string, lastname string, uid string)
 	return token, refreshtoken, err
 }
 
-func ValidateToken() {
+func ValidateToken(signedtToken string) (claims *SignedDetails, msg string) {
+	token,err := jwt.ParseWithClaims(signedtToken,&SignedDetails{},func(token *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY),nil
+	})
 
+	if err != nil {
+		msg = err.Error()
+		return 
+	}
+
+	claims,ok := token.Claims.(*SignedDetails)
+	if !ok{
+		msg = "the token is invalid"
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+		msg = "Token is already expired"
+		return
+	}
+	return claims,msg
 }
 
-func UpdateAllTokens() {
+func UpdateAllTokens(signedtoken string,signedrefreshtoken string,userid string) {
+	var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
 
+	var updateobj primitive.D
+
+	updateobj = append(updateobj,bson.E{Key:"Token",Value:signedtoken})
+	updateobj = append(updateobj,bson.E{Key: "refresh_token",Value: signedrefreshtoken})
+	updated_at,_ := time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+	updateobj = append(updateobj, bson.E{Key: "updatedat",Value: updated_at})
+
+	upsert := true
+	filter:=bson.M{"user_id":userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_,err := UserData.UpdateOne(ctx,filter,bson.D{
+		{Key:"$set",Value: updateobj},
+	},&opt)
+
+	defer cancel()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 }
