63342127d06bb23ba6a5e93bbf3600d48df41b84
Anish <119729668+Anish2545@users.noreply.github.com>
2024-08-14 13:12:08 +0000

    fixes

--- controllers/cart.go
+++ controllers/cart.go
@@ -88,7 +88,7 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
+		err = database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
 
 		if err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, err)
